98dcbd6dff125a428cf5e1b0be3f54573cd572ad
Jethro Dean <jethroldean@gmail.com>
2022-09-25 12:00:22 +1000

    destination functionality implemented

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,8 +1,9 @@
 import { StyleSheet, Text, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import Map from "../components/Map";
-import MapView from "react-native-maps";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import NavigateCard from "../components/NavigateCard";
+import RideOptionsCard from "../components/RideOptionsCard";
 
 const MapScreen = () => {
   const Stack = createNativeStackNavigator();
@@ -17,6 +18,13 @@ const MapScreen = () => {
           <Stack.Screen
             name="NavigateCard"
             component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="RideOptionsCard"
+            component={RideOptionsCard}
             options={{
               headerShown: false,
             }}
